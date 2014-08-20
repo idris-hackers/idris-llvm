0f61431335a8de46d1db895b8c99c7c65bfa0031
Markus Pfeiffer <markus.pfeiffer@morphism.de>
2014-08-20 12:18:42 +0100

    Seperate out LLVM backend

---
+++ Main.hs
@@ -0,0 +1,44 @@
+module Main where
+
+import Idris.Core.TT
+import Idris.AbsSyntax
+import Idris.ElabDecls
+import Idris.REPL
+
+import IRTS.Compiler
+import IRTS.CodegenLLVM
+
+import System.Environment
+import System.Exit
+
+import Paths_idris
+
+data Opts = Opts { inputs :: [FilePath],
+                   output :: FilePath }
+
+showUsage = do putStrLn "Usage: idris-llvm <ibc-files> [-o <output-file>]"
+               exitWith ExitSuccess
+
+getOpts :: IO Opts
+getOpts = do xs <- getArgs
+             return $ process (Opts [] "a.out") xs
+  where
+    process opts ("-o":o:xs) = process (opts { output = o }) xs
+    process opts (x:xs) = process (opts { inputs = x:inputs opts }) xs
+    process opts [] = opts
+
+llvm_main :: Opts -> Idris ()
+llvm_main opts = do elabPrims
+                 loadInputs (inputs opts) Nothing
+                 mainProg <- elabMain
+                 ir <- compile (Via "llvm") (output opts) mainProg
+                 runIO $ codegenLLVM ir
+
+main :: IO ()
+main = do opts <- getOpts
+          if (null (inputs opts)) 
+             then showUsage
+             else runMain (llvm_main opts)
+
+
+
